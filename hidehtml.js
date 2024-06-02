c0384ece57018ac6c551d7737b10bfbbb13ea6f0
Yue <loli.chan.4.4a@gmail.com>
2024-06-02 23:56:29 +0200

    Delete hidehtml.js

--- hidehtml.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var path = window.location.pathname;
-    if (path.endsWith(".html")) {
-        var newPath = path.slice(0, -5);
-        window.history.replaceState(null, "", newPath);
-    }
-});
